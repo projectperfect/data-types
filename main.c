c8c467332c43ca2916be174d426f3e23c6533bed
Perfect <110018708+projectperfect@users.noreply.github.com>
2024-05-06 23:26:52 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int integerVar = 100;
+    float floatingVar = 331.79;
+    double doubleVar = 8.44e+11;
+
+    // _Bool boolVar = 0;
+    bool myBoolean = true;
+    bool anotherBoolean = false; 
+
+
+
+    return 0;
+}
